de62d9d1f27dd0bcc12c3cb7d726bd69a39f50ed
norbert barics <norbert.barics@gmail.com>
2017-04-18 11:34:42 +0200

    odd average method ready

--- blackjack/src/Deck.java
+++ blackjack/src/Deck.java
@@ -5,12 +5,14 @@ public class Deck {
   List<Card> deck = new ArrayList<>();
   List<String> cardValues = new ArrayList<>(
           Arrays.asList("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"));
+
   List<String> cardColors = new ArrayList<>(
           Arrays.asList("Clubs", "Diamonds", "Hearts", "Spades"));
   List<String> cardList = new ArrayList<>();
 
   public Deck(int cardNumber) {
     this.cardNumber = cardNumber;
+
     int Colors = 0;
     for (int j = 0; j < 4; j++) {
       Colors = j;
@@ -23,7 +25,7 @@ public class Deck {
     }
 
     String temp = "";
-    for (int k = 0; k < deck.size();k++){
+    for (int k = 0; k < deck.size(); k++) {
 
       temp = deck.get(k).toString();
       cardList.add(deck.get(k).toString());
---
+++ oddavg/src/OddAvg.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class OddAvg {
+
+  public static void main(String[] args) {
+    List<Integer> numberList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    OddAvg(numberList);
+    System.out.println(OddAvg(numberList));
+  }
+
+
+  static int OddAvg(List<Integer> numbers) {
+    List<Integer> oddList = new ArrayList<>();
+    int averageNumber = 0;
+    for (int i = 0; i < numbers.size(); i++) {
+      if (numbers.get(i) % 2 != 0) {
+        oddList.add(numbers.get(i));
+      }
+    }
+    for (int j = 0; j < oddList.size(); j++) {
+      averageNumber += oddList.get(j);
+    }
+
+    return averageNumber/ oddList.size();
+  }
+}
