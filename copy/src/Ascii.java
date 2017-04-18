e705295cd430915ed104091a48b9291bcea8800b
norbert barics <norbert.barics@gmail.com>
2017-04-18 11:15:51 +0200

    updated gitignore with *.csv, copy class basic functions working

---
+++ copy/src/Ascii.java
@@ -0,0 +1,21 @@
+public class Ascii {
+  public void printAscii (){
+    System.out.println("          )   (        )  \n" +
+            "   (   ( /(   )\\ )  ( /(  \n" +
+            "   )\\  )\\()) (()/(  )\\()) \n" +
+            " (((_)((_)\\   /(_))((_)\\  \n" +
+            " )\\___  ((_) (_)) __ ((_) \n" +
+            "((/ __|/ _ \\ | _ \\\\ \\ / / \n" +
+            " | (__| (_) ||  _/ \\ V /  \n" +
+            "  \\___|\\___/ |_|    |_|   \n" +
+            "                          \n");
+  }
+
+  public void printUsage() {
+    System.out.println("JAVA Copy application\n" +
+            "=======================\n" +
+            "you should have two arguments\n" +
+            "the first is the source\n" +
+            "the second is the destination!");
+  }
+}
---
+++ copy/src/Copy.java
@@ -0,0 +1,65 @@
+import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class Copy {
+  private static final String LINE = "=======================================\n";
+  private static final String NODESTINAITON = "No destination provided\n";
+  private static final String ERROR = "SYNTAX ERROR: ";
+  private static String source;
+  private static String destination;
+
+
+
+  public static void main(String[] args) {
+    source = args[0];
+    destination = args[1];
+    Ascii logo = new Ascii();
+    if (args.length == 0) {
+      logo.printAscii();
+      logo.printUsage();
+    } else if (args.length == 1){
+      System.out.println(LINE + NODESTINAITON + LINE);
+      logo.printAscii();
+      logo.printUsage();
+    } else if (args.length == 2) {
+      List<String> file = readFiles();
+      readFiles();
+      writeToFile(file);
+      logo.printAscii();
+    } else {
+      lineBreak();
+      logo.printUsage();
+    }
+  }
+
+  private static List<String> readFiles() {
+    List<String> rawLines = new ArrayList<>();
+    Path myPath = Paths.get(source);
+    try {
+      rawLines = Files.readAllLines(myPath);
+    } catch (IOException ex) {
+      System.out.println(ERROR + " READ");
+    }
+    return rawLines;
+  }
+
+  private static void writeToFile(List<String> data) {
+    List<String> file = readFiles();
+    Path myPath = Paths.get(destination);
+    try {
+      Files.write(myPath, file);
+    } catch (IOException ex) {
+      System.out.println(ERROR + " WRITE");
+    }
+  }
+
+  private static void lineBreak() {
+    System.out.println("");
+  }
+}
+
+
